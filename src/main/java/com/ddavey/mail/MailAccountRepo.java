4d270fdaac50732d8bc8800e5d3bb3c46e8ea691
ddavey <ddavey@192.168.0.4>
2018-11-07 21:36:31 +0000

    mail account repo

--- src/main/java/com/ddavey/mail/MailAccountRepo.java
+++ src/main/java/com/ddavey/mail/MailAccountRepo.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.QueryResultPage;
 
 @Service
 public class MailAccountRepo {
 	@Autowired
 	private DynamoDBMapper dynamodb;
 
-	public void list(DynamoDBScanExpression expression) {
-		dynamodb.scanPage(clazz, expression);
+	public QueryResultPage<MailAccount> list(DynamoDBQueryExpression<MailAccount> expression) {
+		return dynamodb.queryPage(MailAccount.class, expression);
 	}
 }
--- src/main/java/com/ddavey/mail/Repo.java
+++ src/main/java/com/ddavey/mail/Repo.java
@@ -3,16 +3,15 @@ package com.ddavey.mail;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 
-public abstract class Repo<T extends Object> {
+public abstract class Repo<T> {
 	private DynamoDBMapper dynamodb;
 	private Class<T> cls;
 
-	protected Repo(DynamoDBMapper dynamodb, Class<T> cls) {
+	protected Repo(DynamoDBMapper dynamodb) {
 		this.dynamodb = dynamodb;
-		this.cls = cls;
 	}
 
 	public <T> void list(DynamoDBQueryExpression<T> expression) {
-		dynamodb.query(cls, expression);
+		// dynamodb.query(expression., expression);
 	}
 }
